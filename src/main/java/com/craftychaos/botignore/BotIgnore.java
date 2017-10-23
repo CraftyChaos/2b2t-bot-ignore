38f63fd6176422bc26f9999f5d0c896bfe6a473b
CraftyChaos <33003919+CraftyChaos@users.noreply.github.com>
2017-10-22 20:27:46 -0400

    Updated Version

--- src/main/java/com/craftychaos/botignore/BotIgnore.java
+++ src/main/java/com/craftychaos/botignore/BotIgnore.java
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = BotIgnore.MODID, version = BotIgnore.VERSION)
 public class BotIgnore {
 	public static final String MODID = "botignore";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	
 	public static final Integer updateInterval = 5;
 	public static final String listUrl = "https://raw.githubusercontent.com/CraftyChaos/2b2t-bot-ignore/master/signatures/block.txt";
