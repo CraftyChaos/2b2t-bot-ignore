9b240ae5eb4746a8d944d64844f8f1c1bc4403ed
CraftyChaos <33003919+CraftyChaos@users.noreply.github.com>
2017-10-23 17:29:49 -0400

    Updated some build items

--- src/main/java/com/craftychaos/botignore/BotIgnore.java
+++ src/main/java/com/craftychaos/botignore/BotIgnore.java
@@ -9,23 +9,24 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = BotIgnore.MODID, version = BotIgnore.VERSION)
+@Mod(modid = BotIgnore.MODID, version = BotIgnore.VERSION, updateJSON = BotIgnore.UPDATE)
 public class BotIgnore {
 	private final static Minecraft minecraft = Minecraft.getMinecraft();
 	public static final String MODID = "botignore";
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "latest";
+  public static final String UPDATE = "https://raw.githubusercontent.com/CraftyChaos/2b2t-bot-ignore/master/update.json";
 	public static final Integer updateInterval = 5;
 	public static final String listUrl = "https://raw.githubusercontent.com/CraftyChaos/2b2t-bot-ignore/master/signatures/block.txt";
 	public static final Logger logger = LogManager.getLogger(MODID);
 	public static BotList blockList;
-	
+
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		BotIgnore.logger.info("Loading BotIgnore");
 		BotIgnore.blockList = new BotList(updateInterval, listUrl);
 		MinecraftForge.EVENT_BUS.register(new ChatListener());
 	}
-	
+
 	public static Minecraft getMinecraft() {
 		return minecraft;
 	}
